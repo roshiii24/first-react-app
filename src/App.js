96c9ae8ff4da8a4728b2c4fb90e5af1ca5dd29ac
roshiii24 <rrsaggarwal@gmail.com>
2022-09-07 13:25:09 +0530

    Initialize react app and learn about state & prop

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import ClassComponent from './Components/ClassComponent';
+import FunctionalComponent from './Components/FunctionComponent';
+import { useState } from 'react';
 
 function App() {
+
+  const company = "xyz"
+  const [name, setName] = useState("Roshni");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Class Component</h1>
+      <ClassComponent />
+      <br />
+      <h1>Functional Component</h1>
+      <FunctionalComponent name={name} age={21} company={company} setName={setName} />
     </div>
   );
 }
---
+++ src/Components/FunctionComponent.jsx
@@ -0,0 +1,25 @@
+import React,{useState} from 'react';
+
+const FunctionalComponent = (props) => {
+
+    const [count, setCount] = useState(0);
+    const [changeName, setChangeName] = useState("");
+
+    const { name, age, company, setName } = props;
+
+    return (
+        <div>
+            <p>This is Functional Component</p>
+            <button onClick={() => setCount(count + 1)}>Click me to Add 1</button>
+            <button onClick={() => setCount(count - 1)}>Click me to Subtract 1</button>
+            <h1>{count}</h1>
+            <h1>
+                My name is {name} I am {age} years old. I work at {company}
+            </h1>
+            <input type="text" onChange={(e) => setChangeName(e.target.value)} />
+            <button onClick={() => setName(changeName)}>Change Name</button>
+        </div>
+    );
+}
+
+export default FunctionalComponent;
