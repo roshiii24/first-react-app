1883581fb05ad8227ecc8b935cbea16c0666e543
roshiii24 <rrsaggarwal@gmail.com>
2022-09-09 14:21:59 +0530

    completed basics

--- src/pages/ClassComponent.jsx
+++ src/pages/ClassComponent.jsx
@@ -2,10 +2,44 @@ import React from "react";
 import BaseHoc from "../hoc/BaseHoc";
 
 class ClassComponent extends React.Component{
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: "Roshni",
+            age: 21,
+        }
+
+        console.log("Constructor");
+    }
+
+    componentDidMount() {
+        console.log("Component did mount");
+    }
+
+    componentDidUpdate() {
+        console.log("Component did update");
+    }
+
+    componentWillUnmount() {
+        console.log("Component did unmount");
+    }
+
     render() {
+
+        console.log("Render")
+
         return (
             <div>
                 <p>This is Class Component</p>
+                <button
+                    onClick={
+                        () => this.setState({
+                            ...this.state,
+                            age: this.state.age +1,
+                        })
+                    }>Add 1 to age</button>
+                <h1>{this.state.age}</h1>
             </div>
         );
     }
--- src/pages/FunctionComponent.jsx
+++ src/pages/FunctionComponent.jsx
@@ -1,4 +1,4 @@
-import React,{useState} from 'react';
+import React,{useState, useEffect, useRef} from 'react';
 import BaseHoc from '../hoc/BaseHoc';
 
 const FunctionalComponent = (props) => {
@@ -8,11 +8,41 @@ const FunctionalComponent = (props) => {
 
     const { name, age, company, setName } = props;
 
+    // const prevRef = useRef();
+
+    //behaves as a component did mount
+    useEffect(()=>{
+        console.log("Component did mount");
+    }, [])
+    
+    //behaves as a component did update
+    useEffect(() => {
+        console.log("Component did update")
+    })
+
+    useEffect(() => {
+        console.log("Only on changing name")
+    }, [changeName])
+    
+    useEffect(() => {
+        console.log("Only on props change")
+    }, [props])
+    
+    // useEffect(() => {
+    //     console.log(prevRef === count);
+    // },[count])
+
+    //behaves as component did unmount
+    useEffect(()=>{
+        return console.log("Component did unmount");
+    }, [])
+
     return (
         <div>
             <p>This is Functional Component</p>
             <button onClick={() => setCount(count + 1)}>Click me to Add 1</button>
             <button onClick={() => setCount(count - 1)}>Click me to Subtract 1</button>
+            {/* <h1 ref={prevRef}>{count}</h1> */}
             <h1>{count}</h1>
             <h1>
                 My name is {name} I am {age} years old. I work at {company}
